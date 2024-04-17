ea678fce8b34ac338182a2bb67f54d61c8befa52
G85 <gabrielgg85@gmail.com>
2024-04-17 22:14:41 +0100

    Update fibo.asm
    Primera version que imprime el numero 12345 en consola y finaliza el programa.

--- fibo.asm
+++ fibo.asm
@@ -11,7 +11,7 @@ segment DATOS
 	cadena DB '                           $'    ; acaba en $ para que la rutina pare donde encuentra el $
 	indice DW 0
 	resto  DW 0
-	nextCociente DW 12345
+	nextCociente DW 12345 ; (int) usamos un DWORD para guardar valores
 
 segment PILA stack
 		resb 256
@@ -23,41 +23,43 @@ segment CODIGO
 ..start:
 
 GET_NUMBER:
-
-	MOV AX,[nextCociente] ;12345 ; 
+	XOR AX,AX
+	MOV AX,DATOS
+	MOV DS,AX
+	MOV AX,[nextCociente] ;12345 ;
 	XOR CX,CX             ; ponemos CX a 0 para ir guardando cuantos elementos van a la PILA
+	MOV CX,[indice]
 
 	; DIVISION
-	MOV BX,10;   		; Dividimos 80/10
-	XOR DX,DX           ; aqui irá el resto
-	DIV BX              ; dividimos 12345 / BX (5)
-	MOV [resto],DX      ; guardamos en resto el valor de resto 
-	INC CX              ; incrementamos CX para ir guardando cuantos numeros van
-	MOV [indice],CX     ; guardo en indice las iteraciones
-    MOV [nextCociente],AX ; guardamos el cociente actual en nextCociente
+		MOV BX,10;   		; Dividimos 12345/10
+		XOR DX,DX           ; aqui irá el resto
+		DIV BX              ; dividimos 12345 / BX (5)
+		MOV [nextCociente], AX; segun dividimos metemos en nextCociente el cociente
+		MOV [resto],DX      ; guardamos en resto el valor de resto 
+		INC CX              ; incrementamos CX para ir guardando cuantos numeros van
+		MOV [indice],CX     ; guardo en indice las iteraciones
+		
+		
+
 	; FIN DIVISION
 	
 	; GUARDAR DIGITO ASCII EN variable CADENA
-	XOR AX,AX       ; ponemos AX a cero
-	MOV AX,DATOS    ; guardamos los restos en la variable cadena para luego imprimirla
-	MOV DS,AX       ; METEMOS EN DS EL SEGMENTO DE LA VARAIBLE cadena
-	ADD DX,'0'      ; sumamos el 0 . 48 en decimal que es el 0 en ascii.
-	MOV BX,[indice] ; ponemos en BX el valor de INDICE 0,1,2,3... para ir avanzando en cadena
-	MOV [cadena + BX],DX ; GUARDAMOS EL VALOR DEL RESTO QUE ESTÁ EN PILA EN LA VARAIBLE
+		XOR AX,AX       		; ponemos AX a cero
+		MOV AX,DATOS    		; guardamos los restos en la variable cadena para luego imprimirla
+		MOV DS,AX       		; METEMOS EN DS EL SEGMENTO DE LA VARAIBLE cadena
+		ADD DX,'0'      		; sumamos el 0 . 48 en decimal que es el 0 en ascii.
+		MOV BX,[indice] 		; ponemos en BX el valor de INDICE 0,1,2,3... para ir avanzando en cadena
+		MOV [cadena + BX],DX 	; GUARDAMOS EL VALOR 
 	; FIN GUARDAR DIGITO ASCII EN variable CADENA
 	
+	; PONEMOS DE NUEVO AX con el valor del cociente actual
+	MOV AL,[resto] 					; ¿ Quiza no haga falta ?
+	CMP AL,0
+	JE PRINT_NUMBER
+	JNE GET_NUMBER
+
 	
-	CALL PRINT_NUMBER
-	
-	; PUSH AX
-	; MOV AX,[resto]
-	; CMP AX,0
-	; JNE GET_NUMBER
-	; POP AX
-	; CALL PRINT_NUMBER
-	
-	
-	
+FIN:	
 	;fin programa
 	MOV AH,4Ch
 	INT 21h
@@ -80,5 +82,7 @@ PRINT_NUMBER:
 	POP CX
 	POP BX
 	POP AX
+	
+	CALL FIN
 
 	RET
