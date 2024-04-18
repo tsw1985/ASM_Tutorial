30fbdc1fc0618bad5c878ae4bec00f1365fe5f64
G85 <gabrielgg85@gmail.com>
2024-04-18 09:44:24 +0100

    Update fibop.asm
    Ya mostramos la cadena correctamente. Pero aparece un 0 en el principio.

--- fibop.asm
+++ fibop.asm
@@ -10,8 +10,9 @@
 
 segment DATOS
 	cadena DB '                           $'    ; acaba en $ para que la rutina pare donde encuentra el $
-	indice DW 0
+	contador DW 0
 	resto  DW 0
+	contadorParaCadena DW 0
 	nextCociente DW 12345 ; (int) usamos un DWORD para guardar valores
 
 segment PILA stack
@@ -23,43 +24,56 @@ segment CODIGO
 
 ..start:
 
+MOV CX,0 ; inicia CX a 0
 GET_NUMBER:
 	XOR AX,AX
 	MOV AX,DATOS
 	MOV DS,AX
 	MOV AX,[nextCociente] ;12345 ;
-	XOR CX,CX             ; ponemos CX a 0 para ir guardando cuantos elementos van a la PILA
-	MOV CX,[indice]
+	
 
 	; DIVISION
-		MOV BX,10;   		; Dividimos 12345/10
-		XOR DX,DX           ; aqui irá el resto
-		DIV BX              ; dividimos 12345 / BX (5)
-		MOV [nextCociente], AX; segun dividimos metemos en nextCociente el cociente
-		PUSH DX  
-		MOV [resto],DX      ; guardamos en resto el valor de resto 
-		INC CX              ; incrementamos CX para ir guardando cuantos numeros van
-		MOV [indice],CX     ; guardo en indice las iteraciones
-		
-		
-
-	; FIN DIVISION
+	MOV BX,10;   		; Dividimos 12345/10
+	XOR DX,DX           ; aqui irá el resto
+	DIV BX              ; dividimos 12345 / BX (10)
+	MOV [nextCociente], AX; segun dividimos metemos en nextCociente el cociente
 	
-	; GUARDAR DIGITO ASCII EN variable CADENA
-		XOR AX,AX       		; ponemos AX a cero
-		MOV AX,DATOS    		; guardamos los restos en la variable cadena para luego imprimirla
-		MOV DS,AX       		; METEMOS EN DS EL SEGMENTO DE LA VARAIBLE cadena
-		ADD DX,'0'      		; sumamos el 0 . 48 en decimal que es el 0 en ascii.
-		MOV BX,[indice] 		; ponemos en BX el valor de INDICE 0,1,2,3... para ir avanzando en cadena
-		MOV [cadena + BX],DX 	; GUARDAMOS EL VALOR 
-	; FIN GUARDAR DIGITO ASCII EN variable CADENA
+	PUSH DX             ; guardamos resto en PILA que está en DX
+	MOV [resto],DX      ; y tambien lo guardamos en la variable RESTO
+	
+	MOV CX,[contador]
+	INC CX              ; incrementamos CX para ir guardando cuantos numeros van
+	MOV [contador],CX     ; guardo en contador las iteraciones
 	
 	; PONEMOS DE NUEVO AX con el valor del cociente actual
-	MOV AL,[resto] 					; ¿ Quiza no haga falta ?
-	CMP AL,0
-	JE PRINT_NUMBER
-	JNE GET_NUMBER
+	MOV AL,[resto] 					
+	CMP AL,0 			; ¿ ya el resto es 0 ?
+	JE PROCESO_IMPRIMIR_NUMERO; ¿ si ? pues vamos a sacar los numeros
+	JNE GET_NUMBER      ; ¿ no ? sigue dividiendo
 
+PROCESO_IMPRIMIR_NUMERO:
+
+	XOR CX,CX
+	MOV CX,[contador] ; ponemos el contador de CX con el total de iteraciones de division
+				      ; ya que la instruccion LOOP necesita que CX tenga el numero de iteraciones
+
+SACA_RESTO: ; GUARDAR DIGITO ASCII EN variable CADENA
+	
+	XOR AX,AX       		; ponemos AX a cero
+	MOV AX,DATOS    		; guardamos los restos en la variable cadena para luego imprimirla
+	
+	POP DX
+	
+	MOV DS,AX       		; METEMOS EN DS EL SEGMENTO DE LA VARAIBLE cadena
+	ADD DX,'0'      		; sumamos el 0 . 48 en decimal que es el 0 en ascii.
+	MOV BX,[contadorParaCadena] ; ponemos en BX el valor de contador 0,1,2,3... para ir avanzando en cadena
+	MOV [cadena + BX],DX 	; GUARDAMOS EL VALOR 
+	
+	INC BX                  ;incrementamos BX para que en la siguiente vuelta sea +1
+	MOV [contadorParaCadena],BX ; guardamos en el contador su numero valor incrementado +1
+LOOP SACA_RESTO
+
+CALL PRINT_NUMBER
 	
 FIN:	
 	;fin programa
